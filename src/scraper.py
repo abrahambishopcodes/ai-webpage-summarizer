9cf7b65f1f927d3ef883dcd7e4e9b413850dda43
abrahambishopcodes <crypticcodetechnologies@gmail.com>
2026-09-02 13:15:05 +0100

    feat: added dynamic scraper with selenium

--- src/scraper.py
+++ src/scraper.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 def scrape(url):
     """
-    Scrapes the content of the given URL and returns the HTML content.
+    Scrapes the content of the given URL ( static page ) and returns the HTML content.
 
     Args:
         url (str): The URL to scrape.
@@ -21,3 +22,33 @@ def scrape(url):
     except requests.exceptions.RequestException as e:
         print(f"Error occurred while scraping {url}: {e}")
         return None
+
+
+def scrape_with_selenium(url):
+    """
+    Scrapes the content of web pages which makes use of javascript to load the content, it waits 
+    for the content to be available before fetching
+
+    Args:
+        url (string): The url of the web page to scrape
+    """
+    
+    print(f"Scraping URL: {url}")
+
+    driver = webdriver.Chrome()
+
+    try:
+
+        driver.get(url)
+        driver.implicitly_wait(10)
+
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+
+        title = soup.title.text if soup.title else "No title found"
+        text = soup.get_text(" ", strip=True)
+
+        return title, text
+
+    finally:
+        driver.quit() # Quit the driver after scraping to free up resources
